174b45da31137147aecf74cd7c390d20ccc79410
Tulus1804 <tulus0776@gmail.com>
2020-10-01 00:49:42 +0700

    edited

--- application/views/dashboard/about.php
+++ application/views/dashboard/about.php
@@ -1,1 +1,118 @@
-<?= 'halaman tentang' ?>
+<div class="fh5co-loader"></div>
+
+<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="top-menu">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-1">
+						<div id="fh5co-logo"><a href="<?= base_url() ?>"><img src="<?= base_url() ?>/assets/img/icon/logo-exam.png" alt="logo" width="100px" height="50px"><span></span></a></div>
+					</div>
+					<div class="col-xs-11 text-right menu-1">
+						<ul>
+							<li class="active"><a href="<?= base_url() ?>">Home</a></li>
+							<li><a href="<?= base_url('about') ?>">Tentang</a></li>
+							<li><a href="<?= base_url('help') ?>">Bantuan</a></li>
+							<li class="btn-cta"><a href="<?= base_url('auth') ?>"><span>Log In</span></a></li>
+						</ul>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</nav>
+
+	<!-- Content -->
+	<header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner" style="background-image:url(<?= base_url() ?>assets/dist/learn/images/bg.jpg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>Tentang</h1>
+							<p>Aplikasi Ujian Online Berbasis Web dengan Pengacakan Soal per Bab</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="fh5co-blog">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+					<h2>Aplikasi Ujian Online Berbasis Web dengan Pengacakan Soal per Bab</h2>
+					<p>Merupakan hasil implementasi metode ujian online</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-4">
+					<div class="fh5co-blog animate-box">
+						<a href="#"><img class="img-responsive" src="images/project-4.jpg" alt=""></a>
+						<div class="blog-text">
+							<h3><a href="" #>45 Minimal Workspace Rooms for Web Savvys</a></h3>
+							<span class="posted_on">Nov. 15th</span>
+							<span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+							<a href="#" class="btn btn-primary">Read More</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4">
+					<div class="fh5co-blog animate-box">
+						<a href="#"><img class="img-responsive" src="images/project-2.jpg" alt=""></a>
+						<div class="blog-text">
+							<h3><a href="" #>45 Minimal Worksspace Rooms for Web Savvys</a></h3>
+							<span class="posted_on">Nov. 15th</span>
+							<span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+							<a href="#" class="btn btn-primary">Read More</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4">
+					<div class="fh5co-blog animate-box">
+						<a href="#"><img class="img-responsive" src="images/project-3.jpg" alt=""></a>
+						<div class="blog-text">
+							<h3><a href="" #>45 Minimal Workspace Rooms for Web Savvys</a></h3>
+							<span class="posted_on">Nov. 15th</span>
+							<span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+							<a href="#" class="btn btn-primary">Read More</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<footer id="fh5co-footer" role="contentinfo">
+		<div class="container">
+
+			<div class="row copyright">
+				<div class="col-md-12 text-center">
+					<p>
+						<small class="block">&copy; <?= date('Y'); ?> TA.2008</small>
+						<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.com/" target="_blank">Unsplash</a></small>
+					</p>
+					<!-- <p>
+						<ul class="fh5co-social-icons">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						</ul>
+					</p> -->
+				</div>
+			</div>
+
+		</div>
+	</footer>
+</div>
+
+<div class="gototop js-top">
+	<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+</div>
--- application/views/dashboard/help.php
+++ application/views/dashboard/help.php
@@ -1,1 +1,92 @@
-<?= 'halaman bantuan' ?>
+<div class="fh5co-loader"></div>
+
+<div id="page">
+	<nav class="fh5co-nav" role="navigation">
+		<div class="top-menu">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-1">
+						<div id="fh5co-logo"><a href="<?= base_url() ?>"><img src="<?= base_url() ?>/assets/img/icon/logo-exam.png" alt="logo" width="100px" height="50px"><span></span></a></div>
+					</div>
+					<div class="col-xs-11 text-right menu-1">
+						<ul>
+							<li class="active"><a href="<?= base_url() ?>">Home</a></li>
+							<li><a href="<?= base_url('about') ?>">Tentang</a></li>
+							<li><a href="<?= base_url('help') ?>">Bantuan</a></li>
+							<li class="btn-cta"><a href="<?= base_url('auth') ?>"><span>Log In</span></a></li>
+						</ul>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</nav>
+
+	<!-- Content -->
+	<header id="fh5co-header" class="fh5co-cover fh5co-cover-sm" role="banner" style="background-image:url(<?= base_url() ?>assets/dist/learn/images/bg.jpg);" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center">
+					<div class="display-t">
+						<div class="display-tc animate-box" data-animate-effect="fadeIn">
+							<h1>Bantuan</h1>
+							<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<div id="fh5co-explore" class="fh5co-bg-section">
+		<div class="container">
+			<div class="row animate-box">
+				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
+					<h2>Belum punya akun?</h2>
+					<hr>
+				</div>
+			</div>
+		</div>
+		<div class="fh5co-explore fh5co-explore1">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-push-5 animate-box">
+						<img class="img-responsive" src="<?= base_url(); ?>/assets/img/work_1.png" alt="work">
+					</div>
+					<div class="col-md-4 col-md-pull-8 animate-box">
+						<div class="mt">
+							<h3>We Want You To Learn English</h3>
+							<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+							<ul class="list-nav">
+								<li><i class="icon-check2"></i>Far far away, behind the word</li>
+								<li><i class="icon-check2"></i>There live the blind texts</li>
+								<li><i class="icon-check2"></i>Separated they live in bookmarksgrove</li>
+							</ul>
+							<p><a class="btn btn-primary btn-lg popup-vimeo btn-video" href="https://vimeo.com/channels/staffpicks/93951774"><i class="icon-play"></i> Watch Video</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<footer id="fh5co-footer" role="contentinfo">
+			<div class="container">
+
+				<div class="row copyright">
+					<div class="col-md-12 text-center">
+						<p>
+							<small class="block">&copy; <?= date('Y'); ?> TA.2008</small>
+							<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.com/" target="_blank">Unsplash</a></small>
+						</p>
+					</div>
+				</div>
+
+			</div>
+		</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
